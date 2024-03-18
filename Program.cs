a3c06a9b568cc85c3ddd3f29c755902d5e066616
isabellagoes <141466024+isabellagoes@users.noreply.github.com>
2024-03-18 01:23:50 +0000

    .

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+int horas, velKM;
+double distancia, litros;
+
+Console.WriteLine("Considerando um automóvel que faz 12km/l; informe a quantidade de horas gastas e a velcoidade média (em km/h) em uma viagem a fim de efetuar o cálculo do gasto total de litros de combustível: \n");
+
+Console.Write("Horas gastas: ");
+horas = Convert.ToInt16(Console.ReadLine());
+
+Console.Write("Velocidade média (em km/h): ");
+velKM = Convert.ToInt16(Console.ReadLine());
+        
+distancia = velKM * horas;
+litros = distancia / 12;
+		
+Console.WriteLine($"\nForam gastos na viagem {litros.ToString("F3")}l.");
+
